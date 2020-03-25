96a763e6fa10ebd97b93868a730d929bc017f56f
BenKorcz <38218024+BenKorcz@users.noreply.github.com>
2020-03-25 18:44:39 +1030

    Removed old way for pivot

--- Theory and Practice of Computation/Assignment_1/quickSort_Rec.java
+++ Theory and Practice of Computation/Assignment_1/quickSort_Rec.java
@@ -56,20 +56,6 @@ class quickSortArray{
                 j--;
             }
         }
-        // int i = (low-1);
-        // for(int j=low;j<high;j++){
-        //     if(arr[j]<=pivot){
-        //         i++;
-
-        //         int temp = arr[i];
-        //         arr[i] = arr[j];
-        //         arr[j] = temp;
-        //     }
-        // }
-
-        // int temp = arr[i+1];
-        // arr[i+1] = arr[high];
-        // arr[high] = temp;
 
         return i;
     }
